909908202dc062f0672af2c2843e359755016456
Icey <280773309@qq.com>
2016-09-09 20:45:19 +0800

    snow
    snow_demo

---
+++ script.js
@@ -0,0 +1,53 @@
+var snowflakes = [];
+var can = document.getElementById("m-can");
+var ct = can.getContext('2d');
+init();
+// 定时器
+setInterval(function(){
+	snowflakeNum();
+	ct.clearRect(0,0,700,700);
+	init();
+	dropDown();
+},50);
+// 初始化
+function init(){
+	ct.rect(0,0,700,700);
+	ct.fillStyle = '#000';
+	ct.fill();
+}
+// 产生随机雪花
+function createSnow(){
+	var ix = Math.random()*700;
+	var iy = Math.random()*20;
+	var ir = Math.random()*3;
+	var ivy = Math.random()*0.5+1;//????????变化的y????
+	var snowflake = {
+		x:ix,
+		y:iy,
+		r:ir,
+		vy:ivy,
+		// g:8
+	};
+	snowflakes.push(snowflake);
+}
+// 雪花量
+function snowflakeNum(){
+	createSnow();
+}
+// 下落
+function dropDown(){
+	var len = snowflakes.length;
+	for(var i = 0; i<len; i++){
+		drawSnow(snowflakes[i].x,snowflakes[i].y,snowflakes[i].r,ct);
+		// snowflakes[i].v += snowflakes[i].g;
+		snowflakes[i].y+=snowflakes[i].vy;
+	}
+}
+// 画雪花
+function drawSnow(x,y,r,obj){
+	obj.fillStyle = '#fff';
+	obj.beginPath();
+	obj.arc(x,y,r,0,Math.PI*2,true);
+	obj.closePath();
+	obj.fill;
+}
